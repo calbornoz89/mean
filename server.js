8bef190c75e3e5835d72862e344545f5c10e5dd6
Admin <admin@A51-07.local>
2016-07-08 10:10:58 -0500

    coment port

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var 
 
 app.get('/',function(req,res){
 
-res.sendFile(path.join(__dirname)+ '/index.html');
+res.sendFile(path.join(__dirname) + '/index.html');
 
 })
 
 app.set('port',(process.env.PORT || 5000))
 
 app.listen(app.get('port'));
+
 console.log('Here we go!');
