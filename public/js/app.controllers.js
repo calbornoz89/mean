c2d8e21d9684dc26b9c05eead53fbefb0399e0e1
Admin <admin@A51-07.local>
2016-08-05 22:01:17 -0500

    angular filters

--- public/js/app.controllers.js
+++ public/js/app.controllers.js
@@ -1,9 +1,37 @@
 angular.module('pokeApp.controllers',[])
 
+.controller('mainCtrl', function($location) {
+     var vm = this;
+     vm.goTo = function(route){
+       $location.path(route)
+     }
+   })
 
-.controller('loginCtrl', function(){
+
+
+.controller('loginCtrl', function($http,pokemonServices){
   var vm = this;
   vm.message = 'Este es el login';
+
+// vm => viewModel
+//   vm.getPokemons = function(){
+//     $http
+//     // obtener la DB
+//     .get("bd_pokemon/pokemons.json")
+//
+//
+//     .then(function(response){
+//
+// pokemonServices.setPokemons(response.data)
+//
+//       // console.log(response.data[0])
+//
+//     })
+//   }
+//
+// vm.getPokemons();
+
+
 })
 
 .controller('userCtrl', function(){
@@ -11,7 +39,11 @@ angular.module('pokeApp.controllers',[])
   vm.message = 'Este es el Admin de usuario';
 })
 
-.controller('pokemonCtrl', function(){
+.controller('pokemonCtrl', function(pokemonServices){
   var vm = this;
   vm.message = 'Este es el Admin de pokemon';
+  pokemonServices.getPokemons().then(function(response){
+          vm.pokemons = response;
+
+        })
 })
